5a1544cd31127538e1593a696a7032a2c9c4b58b
Henrique Jung <henriquenj@gmail.com>
2022-10-30 16:35:33 +0100

    Adjust objects in the game scene
    Also make the comments more organized

--- workshop-game/main.cpp
+++ workshop-game/main.cpp
@@ -24,6 +24,7 @@ void glfwErrorCallback(int error, const char* description)
 int main()
 {
 
+    // 1) glfw initialization things
     if (glfwInit() == 0) {
         fprintf(stderr, "Failed to initialize GLFW\n");
         return -1;
@@ -42,39 +43,47 @@ int main()
     // Load OpenGL functions using glad
     int version = gladLoadGL(glfwGetProcAddress);
 
-    // setup Box2D world and apply gravity
+    // 2) setup Box2D world and with some gravity
     b2Vec2 gravity;
     gravity.Set(0.0f, -10.0f);
     g_world = new b2World(gravity);
 
-    // create debug draw. This will actually render all the game for us
+    // 3) create debug draw. We will be using the debugDraw visualization to
+    // create our games. Debug draw calls all the OpenGL functions for us.
     g_debugDraw.Create();
     g_world->SetDebugDraw(&g_debugDraw);
 
 
-    // create ground and add it to the world
-    b2BodyDef bodyDef;
-    b2Body* groundBody = g_world->CreateBody(&bodyDef);
+    // 4) some starter objects are created here, such as the ground
+    b2Body* ground;
+    b2EdgeShape ground_shape;
+    ground_shape.SetTwoSided(b2Vec2(-40.0f, 0.0f), b2Vec2(40.0f, 0.0f));
+    b2BodyDef ground_bd;
+    ground = g_world->CreateBody(&ground_bd);
+    ground->CreateFixture(&ground_shape, 0.0f);
 
-    //sample box body
-    b2Body* b1;
-    {
-        b2EdgeShape shape;
-        shape.SetTwoSided(b2Vec2(-40.0f, 0.0f), b2Vec2(40.0f, 0.0f));
+    b2Body* box;
+    b2PolygonShape box_shape;
+    box_shape.SetAsBox(1.0f, 1.0f);
+    b2FixtureDef box_fd;
+    box_fd.shape = &box_shape;
+    box_fd.density = 20.0f;
+    box_fd.friction = 0.1f;
+    b2BodyDef box_bd;
+    box_bd.type = b2_dynamicBody;
+    box_bd.position.Set(-6.0f + 1.0f, 11.25f);
+    box = g_world->CreateBody(&box_bd);
+    box->CreateFixture(&box_fd);
 
-        b2BodyDef bd;
-        b1 = g_world->CreateBody(&bd);
-        b1->CreateFixture(&shape, 0.0f);
-    }
 
-    // glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-    glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+    // This is color of our background in RGB components
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
     // Control the frame rate. One draw per monitor refresh.
     std::chrono::duration<double> frameTime(0.0);
     std::chrono::duration<double> sleepAdjust(0.0);
 
-    //main application loop
+    // 5) main application loop
     while (!glfwWindowShouldClose(g_mainWindow)) {
         // use std::chrono to control frame rate. Objective here is to maintain
         // a steady 60 frames per second (no more, hopefully no less)
@@ -89,33 +98,30 @@ int main()
         // clear previous frame (avoid creates shadows)
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        // run the simulation for one frame
-        float timeStep = 60 > 0.0f ? 1.0f / 60 : float(0.0f);
-
         // enable objects to be draw
         uint32 flags = 0;
         flags += b2Draw::e_shapeBit;
-        flags += b2Draw::e_jointBit;
-        flags += b2Draw::e_aabbBit;
-        flags += b2Draw::e_centerOfMassBit;
-
         g_debugDraw.SetFlags(flags);
+
+        // 6) When we call Step(), we run the simulation for one frame
+        float timeStep = 60 > 0.0f ? 1.0f / 60 : float(0.0f);
         g_world->Step(timeStep, 8, 3); // TODO: explain these parameters
+
+        // 7) Render everything on the screen
         g_world->DebugDraw();
         g_debugDraw.Flush();
-
-        // put the rendered frame on the screen
         glfwSwapBuffers(g_mainWindow);
 
         glfwPollEvents();
 
-        // Throttle to cap at 60 FPS. Which means if it's going to be past
+        // 8) Throttle to cap at 60 FPS. Which means if it's going to be past
         // 60FPS, sleeps a while instead of doing more frames.
         std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
         std::chrono::duration<double> target(1.0 / 60.0);
         std::chrono::duration<double> timeUsed = t2 - t1;
         std::chrono::duration<double> sleepTime = target - timeUsed + sleepAdjust;
         if (sleepTime > std::chrono::duration<double>(0)) {
+            // Make the framerate not go over by sleeping a little.
             std::this_thread::sleep_for(sleepTime);
         }
         std::chrono::steady_clock::time_point t3 = std::chrono::steady_clock::now();
@@ -123,12 +129,13 @@ int main()
 
         // Compute the sleep adjustment using a low pass filter
         sleepAdjust = 0.9 * sleepAdjust + 0.1 * (target - frameTime);
+
     }
 
-
+    // 9) terminate the program if it reaches here
     glfwTerminate();
-
     g_debugDraw.Destroy();
     delete g_world;
+
     return 0;
 }
